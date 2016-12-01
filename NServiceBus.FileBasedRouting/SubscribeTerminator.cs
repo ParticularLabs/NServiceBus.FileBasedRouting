8424b7085f558f94ec95278d962e500f76f82037
Tim Bussmann <tim.bussmann@particular.net>
2016-12-01 16:55:20 +0100

    add pipeline behaviors required for pubsub

---
+++ NServiceBus.FileBasedRouting/SubscribeTerminator.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.Features;
+using NServiceBus.Logging;
+using NServiceBus.Pipeline;
+
+namespace NServiceBus.FileBasedRouting
+{
+    class SubscribeTerminator : PipelineTerminator<ISubscribeContext>
+    {
+        protected override Task Terminate(ISubscribeContext context)
+        {
+            log.Debug($"Subscribe was called for {context.EventType.FullName}. With FileBasedRouting, subscribe operations have no effect and subscribers should be configured in the routing file. If subscribe was not called by you, consider disabling the {nameof(AutoSubscribe)} feature.");
+            return Task.CompletedTask;
+        }
+
+        static readonly ILog log = LogManager.GetLogger<FileBasedRoutingFeature>();
+    }
+}
---
+++ NServiceBus.FileBasedRouting/UnsubscribeTerminator.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.Logging;
+using NServiceBus.Pipeline;
+
+namespace NServiceBus.FileBasedRouting
+{
+    class UnsubscribeTerminator : PipelineTerminator<IUnsubscribeContext>
+    {
+        protected override Task Terminate(IUnsubscribeContext context)
+        {
+            log.Debug($"Unsubscribe was called for {context.EventType.FullName}. With FileBasedRouting, unsubscribe operations have no effect and subscribers should be configured in the routing file.");
+            return Task.CompletedTask;
+        }
+
+        static readonly ILog log = LogManager.GetLogger<FileBasedRoutingFeature>();
+    }
+}
